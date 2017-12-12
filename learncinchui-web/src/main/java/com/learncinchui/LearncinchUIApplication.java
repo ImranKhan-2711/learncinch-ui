afebd5add37586747978d2fcc967cc299963aa70
Mandeep <mandeep.gill.java@gmail.com>
2017-12-12 12:45:25 +0530

    first commit

---
+++ learncinchui-web/src/main/java/com/learncinchui/LearncinchUIApplication.java
@@ -0,0 +1,13 @@
+package com.learncinchui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearncinchUIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearncinchUIApplication.class, args);
+	}
+
+}
